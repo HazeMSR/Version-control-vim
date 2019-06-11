a7e1c4a17721d826ae465bf9054c22ddfd69823c
Aaron <aaron@sbk01.local>
2019-06-11 18:02:16 -0500

    first commit

---
+++ hola_mundo.py
@@ -0,0 +1,16 @@
+import sys
+
+class loquesea:
+    def f(self):
+        return 'hello world'
+def hola():
+    print(sys.version)
+    print("Hola mundo")
+    print("Quiubo")
+
+def main():
+    hola()
+
+if __name__ == "__main__":
+    main()
+
